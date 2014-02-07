29a09c0c56ba9fc4e54658bf9f7749f2b4556deb
nange <463499510@qq.com>
2014-02-07 18:28:16 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,32 @@
 ﻿// Grunt configuration.
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		less: {
-		  development: {
-		    files: {
-		      "public/css/style.css": "less/style.less"
-		    }
-		  }
-		}
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+  	  development: {
+  	    files: {
+  	      "public/css/style.css": "less/style.less"
+  	    }
+  	  }
+    },
+    connect: {
+      server: {
+          options: {
+            base: 'mockup',
+            port: 9000,
+            keepalive: true
+          }
+        }
+      }
+    }
 
-	});
+  });
  
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task(s).
   grunt.registerTask('default', ['less']);
+  grunt.registerTask('server', ['connect']);
  
 };
