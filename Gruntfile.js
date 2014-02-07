6150493135b1a37e9538c75aa8f5541a23ad4771
nange <463499510@qq.com>
2014-02-07 13:29:12 +0800

    更新目录结构

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+﻿// Grunt configuration.
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+		  development: {
+		    files: {
+		      "public/css/style.css": "less/style.less"
+		    }
+		  }
+		}
+
+	});
+ 
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less']);
+ 
+};
