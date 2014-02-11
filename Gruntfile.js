45e6c295187545589dd0ba93297553a2c6d051af
nange <463499510@qq.com>
2014-02-11 23:17:56 +0800

    add watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,19 @@ module.exports = function(grunt) {
           keepalive: true
         }
       }
+    },
+    watch: {
+      scripts: {
+        files: 'less/*.less',
+        tasks: ['less']        
+      }
     }
 
   });
  
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['less']);
